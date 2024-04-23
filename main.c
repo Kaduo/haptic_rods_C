d16b24022ef60ca7b5f08faba698c189363fc754
Balthazar Potet <balthazarpotet@gmail.com>
2024-04-23 11:51:13 +0200

    hsdfj

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include "raylib.h"
-#define TERMINAL    "/dev/ttyUSB0"
+#include "raymath.h"
+#define TERMINAL "/dev/ttyUSB0"
 
 #include "signals.h"
 #include <errno.h>
@@ -12,120 +13,126 @@
 const int NB_RODS_MENU = 10;
 const int UNIT_ROD_WIDTH = 30;
 const int ROD_HEIGHT = 20;
-const Color colors[] = {LIGHTGRAY, RED, GREEN, PURPLE, YELLOW, DARKGREEN, BLACK, BROWN, BLUE, ORANGE};
+const Color colors[] = {LIGHTGRAY, RED,   GREEN, PURPLE, YELLOW,
+                        DARKGREEN, BLACK, BROWN, BLUE,   ORANGE};
 
 typedef struct Rod {
-    Rectangle rect;
-    Color color;
+  Rectangle rect;
+  Color color;
 } Rod;
 
 void DrawRods(Rod rods[], int nbRods) {
-    int i;
-    for (i=0; i<nbRods; i++) {
-        DrawRectangleRec(rods[i].rect, rods[i].color);
-    }
+  int i;
+  for (i = 0; i < nbRods; i++) {
+    DrawRectangleRec(rods[i].rect, rods[i].color);
+  }
 }
 
 void InitRodsMenu(Rod rodsMenu[]) {
-    int i;
-    for (i = 0; i < NB_RODS_MENU; i++) {
-        rodsMenu[i] = (Rod){.rect = {0, i*ROD_HEIGHT, (i+1)*UNIT_ROD_WIDTH, ROD_HEIGHT}, .color= colors[i]};
-    }
-
+  int i;
+  for (i = 0; i < NB_RODS_MENU; i++) {
+    rodsMenu[i] =
+        (Rod){.rect = {0, i * ROD_HEIGHT, (i + 1) * UNIT_ROD_WIDTH, ROD_HEIGHT},
+              .color = colors[i]};
+  }
 }
 
-int main(void)
-{
-    int fd;
-    fd = connect_to_tty();
+int compute_speed(int delta_x, int delta_y, int *old_time) {
+  int new_time = GetFrameTime();
+  int speed;
+  if ((*old_time != 0) && (new_time - *old_time != 0)) {
+    speed = Vector2Length((Vector2){.x = delta_x, .y = delta_y}) /
+            (GetFrameTime() - *old_time);
+  } else {
+    speed = 1000;
+  }
+  *old_time = new_time;
+  return speed;
+}
 
-    ping(fd);
-    set_direction(fd, 0, 300);
+int main(void) {
+  int fd;
+  fd = connect_to_tty();
 
-        unsigned char buf[2];
-        int rdlen;
+  /* ping(fd); */
+  set_direction(fd, 0, 300);
 
-        rdlen = read(fd, buf, sizeof(buf) - 1);
-        if (rdlen > 0) {
-#ifdef DISPLAY_STRING
-            buf[rdlen] = 0;
-            printf("Read %d: \"%s\"\n", rdlen, buf);
-#else /* display hex */
-            unsigned char   *p;
-            printf("Read %d:", rdlen);
-            for (p = buf; rdlen-- > 0; p++)
-                printf(" 0x%x", *p);
-            printf("\n");
-#endif
-        } else if (rdlen < 0) {
-            printf("Error from read: %d: %s\n", rdlen, strerror(errno));
-        } else {  /* rdlen == 0 */
-            printf("Timeout from read\n");
+  /*   unsigned char buf[2]; */
+  /*   int rdlen; */
+
+  /*   rdlen = read(fd, buf, sizeof(buf) - 1); */
+  /*   if (rdlen > 0) { */
+  /* #ifdef DISPLAY_STRING */
+  /*     buf[rdlen] = 0; */
+  /*     printf("Read %d: \"%s\"\n", rdlen, buf); */
+  /* #else /\* display hex *\/ */
+  /*     unsigned char *p; */
+  /*     printf("Read %d:", rdlen); */
+  /*     for (p = buf; rdlen-- > 0; p++) */
+  /*       printf(" 0x%x", *p); */
+  /*     printf("\n"); */
+  /* #endif */
+  /*   } else if (rdlen < 0) { */
+  /*     printf("Error from read: %d: %s\n", rdlen, strerror(errno)); */
+  /*   } else { /\* rdlen == 0 *\/ */
+  /*     printf("Timeout from read\n"); */
+  /*   } */
+
+  Signal signal = signal_new(SINE, 255, 0, 0, 30, 0);
+
+  const int WINDOW_WIDTH = 1024;
+  const int WINDOW_HEIGHT = 600;
+
+  int selected = -1;
+  int deltaX = 0;
+  int deltaY = 0;
+
+  Rod rodsMenu[NB_RODS_MENU];
+  InitRodsMenu(rodsMenu);
+  InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "HapticRods");
+
+  int time;
+  time = 0;
+  // Main loop
+  while (!WindowShouldClose()) {
+    BeginDrawing();
+    ClearBackground(RAYWHITE);
+
+    Vector2 mousePosition = GetMousePosition();
+
+    // Selection logic
+    if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
+      int i;
+      for (i = 0; i < NB_RODS_MENU; i++) {
+        if (CheckCollisionPointRec(mousePosition, rodsMenu[i].rect)) {
+          selected = i;
+          deltaX = rodsMenu[i].rect.x - mousePosition.x;
+          deltaY = rodsMenu[i].rect.y - mousePosition.y;
+
+          printf("hi there ! \n");
+          set_signal(fd, -1, -1, signal);
+          break;
         }
-
-    
-    Signal signal = signal_new(
-        SINE,
-        255,
-        0,
-        0,
-        30,
-        0
-);
-
-
-
-    const int WINDOW_WIDTH = 1024;
-    const int WINDOW_HEIGHT = 600;
-
-    int selected = -1;
-    int deltaX = 0;
-    int deltaY = 0;
-
-    Rod rodsMenu[NB_RODS_MENU];
-    InitRodsMenu(rodsMenu);
-    InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "HapticRods");
-
-    // Main loop
-    while (!WindowShouldClose())
-    {
-        BeginDrawing();
-        ClearBackground(RAYWHITE);
-
-        Vector2 mousePosition = GetMousePosition();
-
-        // Selection logic
-        if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
-            int i;
-            for (i=0; i < NB_RODS_MENU; i++) {
-                if (CheckCollisionPointRec(mousePosition, rodsMenu[i].rect)) {
-                    selected = i;
-                    deltaX = rodsMenu[i].rect.x - mousePosition.x;
-                    deltaY = rodsMenu[i].rect.y - mousePosition.y;
-
-                    printf("hi there ! \n");
-                    set_signal(fd, -1, -1, signal);
-                    break;
-                }
-            }
-        } else if (IsMouseButtonReleased(MOUSE_BUTTON_LEFT)) {
-            selected = -1;
-            clear_signal(fd);
-            play_signal(fd, 0);
-        }
-
-        if (IsMouseButtonDown(MOUSE_BUTTON_LEFT) && selected >= 0) {
-            rodsMenu[selected].rect.x = mousePosition.x + deltaX;
-            rodsMenu[selected].rect.y = mousePosition.y + deltaY;
-        }
-
-        // Draw menu
-        DrawRods(rodsMenu, NB_RODS_MENU);
-
-        EndDrawing();
+      }
+    } else if (IsMouseButtonReleased(MOUSE_BUTTON_LEFT)) {
+      selected = -1;
+      clear_signal(fd);
+      play_signal(fd, 0);
     }
 
-    CloseWindow();
+    if (IsMouseButtonDown(MOUSE_BUTTON_LEFT) && selected >= 0) {
+      rodsMenu[selected].rect.x = mousePosition.x + deltaX;
+      rodsMenu[selected].rect.y = mousePosition.y + deltaY;
+      set_direction(fd, 0, compute_speed(deltaX, deltaY, &time));
+    }
 
-    return 0;
+    // Draw menu
+    DrawRods(rodsMenu, NB_RODS_MENU);
+
+    EndDrawing();
+  }
+
+  CloseWindow();
+
+  return 0;
 }
