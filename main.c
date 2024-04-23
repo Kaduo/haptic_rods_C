7d56ec827d99a3ac0a6f831ad396799c53349474
Balthazar Potet <balthazarpotet@gmail.com>
2024-04-23 10:29:44 +0200

    Quick test

--- main.c
+++ main.c
@@ -71,6 +71,11 @@ int main(void)
         30,
         0
 );
+    unsigned char xstr[11] = {0x83, 0xFF,0xFF,0x02,0xFF,0x00,0x00,0x1E,0x00,0x00,0x00};
+    unsigned char hmm[1] = {0x84};
+
+    write_to_tty(fd, xstr, 11);
+    write_to_tty(fd, hmm, 1);
 
 
 
