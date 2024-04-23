00a92b81ec7cb051e5573781acf053a3e9dcfd2c
Balthazar Potet <balthazarpotet@gmail.com>
2024-04-23 14:35:07 +0200

    sdf

--- main.c
+++ main.c
@@ -94,7 +94,7 @@ int main(void) {
   }
   printf("hello world\n");
 
-  uint8_t l;
+  int l;
   int err;
   const char *amplitude_string_expr;
   const te_expr *amplitude_expr;
@@ -120,7 +120,7 @@ int main(void) {
   /* printf("hmm"); */
 
   Signal signals[NB_RODS_MENU];
-  uint8_t i;
+  int i;
   for (i=0; i<NB_RODS_MENU; i++) {
       l = i;
       printf("hkjskdf\n");
