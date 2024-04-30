af1f42e724fc305f0b16fc62b3305d22da4544e6
Balthazar Potet <balthazarpotet@gmail.com>
2024-04-30 16:01:09 +0200

    sdf

--- main.c
+++ main.c
@@ -282,7 +282,9 @@ int main(void) {
         }
       }
       if (!collided) {
-        set_signal(fd, -1, -1, signals[selected]);
+        add_signal(fd, -1, -1, signals[selected]);
+        play_signal(fd, 1);
+        //set_signal(fd, -1, -1, signals[selected]);
       }
       set_direction(fd, compute_angle(dx, dy), compute_speed(dx, dy, &time));
     }
