abfefbf536a58fb42138d0d98fd2d8b599d2497b
Balthazar Potet <balthazarpotet@gmail.com>
2025-01-14 09:46:35 +0100

    automatically detect the local ip for the websocket

--- main.c
+++ main.c
@@ -795,22 +795,37 @@ void ParseArgs(int argc, char **argv, char **configName, char **specName, char *
 
 int main(int argc, char **argv)
 {
-
+  // Find local IP -->
   const char *host;
 
-  #ifdef DESKTOP
-  host = "localhost";
-  #else
-  host = "192.168.1.24";
-  #endif
+  struct ifaddrs * ifAddrStruct=NULL;
+  struct ifaddrs * ifa=NULL;
+  void * tmpAddrPtr=NULL;
+
+  getifaddrs(&ifAddrStruct);
+
+  for (ifa = ifAddrStruct; ifa != NULL; ifa = ifa->ifa_next) {
+      if (!ifa->ifa_addr) {
+          continue;
+      }
+      if (ifa->ifa_addr->sa_family == AF_INET) { // check it is IP4
+          // is a valid IP4 Address
+
+          tmpAddrPtr=&((struct sockaddr_in *)ifa->ifa_addr)->sin_addr;
+          char addressBuffer[INET_ADDRSTRLEN];
+          inet_ntop(AF_INET, tmpAddrPtr, addressBuffer, INET_ADDRSTRLEN);
+          if (strcmp(ifa->ifa_name, "wlan0") == 0) {
+            printf("My local address is : %s\n", addressBuffer);
+            host = addressBuffer;
+            break;
+          }
+      }
+  }
+  if (ifAddrStruct!=NULL) freeifaddrs(ifAddrStruct);
+  // <-- Find local IP
+
   // Create a websocket
   ws_socket(&(struct ws_server){
-      /*ma
-       * Bind host:
-       * localhost -> localhost/127.0.0.1
-       * 0.0.0.0   -> global IPv4
-       * ::        -> global IPv4+IPv6 (DualStack)
-       */
       .host = host,
       .port = 8080,
       .thread_loop = 1,
@@ -879,29 +894,7 @@ int main(int argc, char **argv)
   ClearAppState(&appState);
   CloseWindow();
 
-    // TODO : find local address automatically
-    //   struct ifaddrs * ifAddrStruct=NULL;
-    // struct ifaddrs * ifa=NULL;
-    // void * tmpAddrPtr=NULL;
-
-    // getifaddrs(&ifAddrStruct);
-
-    // for (ifa = ifAddrStruct; ifa != NULL; ifa = ifa->ifa_next) {
-    //     if (!ifa->ifa_addr) {
-    //         continue;
-    //     }
-    //     if (ifa->ifa_addr->sa_family == AF_INET) { // check it is IP4
-    //         // is a valid IP4 Address
-    //         tmpAddrPtr=&((struct sockaddr_in *)ifa->ifa_addr)->sin_addr;
-    //         char addressBuffer[INET_ADDRSTRLEN];
-    //         inet_ntop(AF_INET, tmpAddrPtr, addressBuffer, INET_ADDRSTRLEN);
-    //         if (strcmp(ifa->ifa_name, "wlan0") == 0) {
-    //           printf("My local address is : %s\n", addressBuffer);
-    //         }
-    //         break;
-    //     }
-    // }
-    // if (ifAddrStruct!=NULL) freeifaddrs(ifAddrStruct);
+  printf("Window closed!\n");
 
   return 0;
 }
